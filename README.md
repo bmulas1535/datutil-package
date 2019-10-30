feb9f8094d466a546043f5fc5e167495bf1008db
bmulas1535 <bmulas1535@users.noreply.github.com>
2019-10-30 13:05:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DATUTIL PACKAGE FOR PYTHON
+----------
+### Main
+This package contains helper functions for common data science processes.
+
+It is still a work in progress, so expect more to be added, and need 
+to be updated frequently. 
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+Setup Documentation
+"""
+import setuptools
+
+
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "scipy",
+]
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    setuptools.setup(
+        name="datutil",
+        version = "1.0.1",
+        author = "bmulas1535",
+        description = "A collection of classes and functions for common data science processes.",
+        long_description = LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        url="https://lambdaschool.com/courses/data-science",
+        packages=setuptools.find_packages(),
+        python_requires=">=3.7",
+        install_requires = REQUIRED,
+        classifiers=["Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ]
+        )
